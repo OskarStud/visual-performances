a5f874273524b5249d4f6eb27bda9fe829633439
Oskar <oskar.stud@gmail.com>
2023-11-14 20:18:17 +0300

    add rectangles

--- rectangle-step/sketch_01.js
+++ rectangle-step/sketch_01.js
@@ -1,13 +1,26 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1000, 1000 ]
+  dimensions: [1000, 1000]
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return (props) => {
+    const { context, width, height } = props;
     context.fillStyle = 'darkblue';
     context.fillRect(0, 0, width, height);
+
+    function drawRect(i) {
+      context.strokeStyle = 'cyan';
+      context.lineWidth = 8;
+      context.strokeRect(400, 200, 50 * i, 50 * i)
+      context.rotate(5 * i / 180)
+    }
+
+    for (let i = 2; i < 10; i += 2) {
+      drawRect(i)
+    }
+
   };
 };
 
