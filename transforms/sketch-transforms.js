311fa6bdb5c24c4603f0a12d5d6c6ff29548bb24
Oskar <oskar.stud@gmail.com>
2023-11-24 14:47:43 +0300

    html draft with simple canvas

--- transforms/sketch-transforms.js
+++ transforms/sketch-transforms.js
@@ -1,14 +1,21 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [1000, 1000]
+  dimensions: [1080, 1080]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'darkred';
     context.fillRect(0, 0, width, height);
+
+    context.save();
+    context.matrix(2, 2, 3, 3, 3, 3);
+    context.fillStyle = 'yellow';
+    context.fillRect(100, 100, 200, 200);
+    context.restore();
   };
 };
 
 canvasSketch(sketch, settings);
+ 
